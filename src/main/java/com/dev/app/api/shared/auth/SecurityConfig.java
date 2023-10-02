48644944d4dd9280a58030ad678aadfb241e50d1
AlexNarvaez00 <zaizealexis@gmail.com>
2023-10-02 17:32:20 -0600

    last changes

--- src/main/java/com/dev/app/api/shared/auth/SecurityConfig.java
+++ src/main/java/com/dev/app/api/shared/auth/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.dev.app.api.shared.auth;
 
 /**
- * El paquete no pudo ser reconocido 
+ * El paquete no pudo ser reconocido, pero lo intente 
  * https://github.com/spring-projects/spring-security/blob/5.4.x/samples/boot/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
  * SecurityConfig
  */
